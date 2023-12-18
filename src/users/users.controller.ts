940d20ef27bf16d6c4c29d2a5b03b9c950c813fb
Luiz Filipe <moresco.luiz@gmail.com>
2023-12-18 18:53:37 +0100

    Make users endpoint behind auth

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,16 @@
-import { Controller, Post, Body, UsePipes, Get, Param } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  UsePipes,
+  Get,
+  Param,
+  UseGuards,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto, schemaCreateUserDto } from './dto/create-user.dto';
 import { ZodValidationPipe } from 'src/pipes/zod-pipe';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -13,11 +22,13 @@ export class UsersController {
     return this.usersService.create(createUserDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   findAll() {
     return this.usersService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(id);
